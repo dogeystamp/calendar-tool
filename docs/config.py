faa23b2fe7bad6352c44ba191b8bfa4b78d26982
DogeyStamp <adeng.sean@gmail.com>
2021-02-27 11:40:53 -0500

    Improve documentation

--- docs/config.py
+++ docs/config.py
@@ -4,8 +4,10 @@ class s_class:
     def __init__(self, name, period,):
         self.name = name
         self.period = period
-
+# If there are two periods that are identical, merge them
 merge_periods = True
+
+# Start times of each period
 start_times = {
   1: datetime.time(8,45,0),
   2: datetime.time(9,30,0),
@@ -15,6 +17,7 @@ start_times = {
   6: datetime.time(13,45,0),
   7: datetime.time(14,45,0),
 }
+# End times of each period
 end_times = {
   1: datetime.time(9,30,0),
   2: datetime.time(10,15,0),
@@ -24,6 +27,7 @@ end_times = {
   6: datetime.time(14,30,0),
   7: datetime.time(15,45,0),
 }
+# Classes that you can put in periods
 classes = [s_class("Gym",1), #1,
            s_class("Maths",1), #2
            s_class("English",1),#3
@@ -35,12 +39,15 @@ classes = [s_class("Gym",1), #1,
            s_class("Geography",1), #9
            s_class("Drama",1),#10
            s_class("Music",1)] #11
+# Which classes you put in each period for each day in the cycle
 schedule_dict = {1:[5,5,7,1,2,8,3],
                 2:[6,6,2,2,3,3,4],
                 3:[9,9,2,2,6,6,5],
                 4:[2,2,5,5,7,7,3],
                 5:[5,5,1,1,10,10,6],
                 6:[5,5,8,8,11,11,9]}
+
+# Start/end times of vacations (Only put the start time if it lasts one day)
 vacation_days = [[datetime.datetime(2020,9,7,0,0,0,0)],
                 [datetime.datetime(2020,10,12,0,0,0,0)],
                 [datetime.datetime(2020,11,2,0,0,0,0),
@@ -54,7 +61,9 @@ vacation_days = [[datetime.datetime(2020,9,7,0,0,0,0)],
                 [datetime.datetime(2021,5,24,0,0,0,0)],
                 [datetime.datetime(2020,11,20,0,0,0,0)],
                 [datetime.datetime(2020,11,27,0,0,0,0)]]
+# Start of the schedule year
 start_date = datetime.datetime(2020, 8, 28, 0, 0, 0, 0)
+# End of the schedule year
 end_date = datetime.datetime(2021, 6, 22, 0, 0, 0, 0)
 #in case you're too lazy to modify the start date or vacation days
 offset = 0
